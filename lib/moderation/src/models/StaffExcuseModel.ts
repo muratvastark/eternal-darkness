605dde852778e3dead93e99ce6af4c93a67d3fb0
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2024-01-14 16:38:57 +0300

    Update and rename StaffExcuse.ts to StaffExcuseModel.ts

--- lib/moderation/src/models/StaffExcuseModel.ts
+++ lib/moderation/src/models/StaffExcuseModel.ts
@@ -3,6 +3,7 @@ import { Schema, model } from 'mongoose';
 
 const staffExcuseSchema = new Schema({
     createdTimestamp: { type: Number, default: () => Date.now() },
+    activity: { type: Boolean, default: true },
     finishTimestamp: { type: Number, default: undefined },
     adminId: String,
     reason: String,
